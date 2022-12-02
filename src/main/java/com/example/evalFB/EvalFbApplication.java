d5258b570a1b5579cbe1979cb98f195959beb7e8
kingMELOw <felix.scaron.57@gmail.com>
2022-12-02 10:35:19 +0100

    buenas tardes

---
+++ src/main/java/com/example/evalFB/EvalFbApplication.java
@@ -0,0 +1,13 @@
+package com.example.evalFB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalFbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalFbApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/evalFB/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.evalFB;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.evalFB.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
